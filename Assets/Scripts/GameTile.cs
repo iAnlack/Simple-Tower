f9164598a7372a63f6d3b4db56aaabe778fcbfc0
Anlack <aka3jiouqpahat@mail.ru>
2021-12-14 00:27:48 +0300

    Initial commit

---
+++ Assets/Scripts/GameTile.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _arrow;
+
+    private GameTile _north, _west, _south, _east, _nextOnPath;
+
+    private int _distance;
+
+    public bool HasPath => _distance != int.MaxValue;
+    public bool IsAlternative { get; set; }
+
+    private Quaternion _northRotation = Quaternion.Euler(90f, 0f, 0f);
+    private Quaternion _eastRotation = Quaternion.Euler(90f, 90f, 0f);
+    private Quaternion _southRotation = Quaternion.Euler(90f, 180f, 0f);
+    private Quaternion _westRotation = Quaternion.Euler(90f, 270f, 0f);          // если ошибка в итоге будет здесь, это трындец.
+
+
+    private GameTileContent _content;
+
+    public GameTile NextTileOnPath => _nextOnPath;
+
+    public Vector3 ExitPoint { get; private set; }
+
+    public Direction PathDirection { get; private set; }
+
+    public GameTileContent Content
+    {
+        get => _content;
+        set
+        {
+            if (_content != null)
+            {
+                _content.Recycle();
+            }
+
+            _content = value;
+            _content.transform.localPosition = transform.localPosition;
+        }
+    }    
+
+    public static void MakeWestEastNeighbors(GameTile east, GameTile west)
+    {
+        west._east = east;
+        east._west = west;
+    }
+
+    public static void MakeNorthSouthNeighbors(GameTile north, GameTile south)
+    {
+        south._north = north;
+        north._south = south;
+    }
+
+    public void ClearPath()
+    {
+        _distance = int.MaxValue;
+        _nextOnPath = null;
+    }
+
+    public void BecomeDestination()
+    {
+        _distance = 0;
+        _nextOnPath = null;
+        ExitPoint = transform.localPosition;
+    }
+
+    private GameTile GrowPathTo(GameTile neighbor, Direction direction)
+    {
+        if (!HasPath || neighbor == null || neighbor.HasPath)
+        {
+            return null;
+        }
+
+        neighbor._distance = _distance + 1;
+        neighbor._nextOnPath = this;
+        neighbor.ExitPoint = neighbor.transform.localPosition + direction.GetHalfVector();
+        neighbor.PathDirection = direction;
+        return neighbor.Content.Type != GameTileContentType.Wall ? neighbor : null;
+    }
+
+    public GameTile GrowPathNorth() => GrowPathTo(_north, Direction.South);
+    public GameTile GrowPathWest() => GrowPathTo(_west, Direction.East);
+    public GameTile GrowPathSouth() => GrowPathTo(_south, Direction.North);
+    public GameTile GrowPathEast() => GrowPathTo(_east, Direction.West);
+
+    public void ShowPath()
+    {
+        if(_distance == 0)
+        {
+            _arrow.gameObject.SetActive(false);
+            return;
+        }
+        _arrow.gameObject.SetActive(true);
+        _arrow.localRotation =
+            _nextOnPath == _north ? _northRotation :
+            _nextOnPath == _east ? _eastRotation :
+            _nextOnPath == _south ? _southRotation :
+            _westRotation;
+    }
+}
